98be4a8172e3e66ec12a5992d5ab68ea00b2cd2b
DustUnderBag <usamary55@gmail.com>
2025-02-10 06:37:44 -0500

    In progress: Fixing scrollable sidebar
    Sidebar's positoin is set to fixed, but still scrolls with the window.

--- src/reset-projects.js
+++ src/reset-projects.js
@@ -11,16 +11,12 @@ export function populateProjects() {
     
     Task.addTask("House Chores", "Wash Dishes", new Date(2025, 1, 21), 2, "Home");
     Task.addTask("Buy Groceries", "Pick up milk, eggs, and vegetables from the supermarket.", new Date(2025, 0, 12), 2, "Home");
-    Task.addTask("Do a weekly review of my goals", "Budget $200", new Date(2025, 0, 1), 3, "Home");
     Task.addTask("Pay Utility Bills", "Settle electricity and water bills before the due date.", new Date(2025, 3, 17), 3, "Home");
 
-    Task.addTask("Call Manager", "Call to discuss project", new Date(2025, 0, 5), 3, "My Work");
     Task.addTask("Organize Workspace", "Clean up desk, sort documents, and declutter unnecessary items.", new Date(2025, 1, 4), 1, "My Work");
-    Task.addTask("Schedule Doctor Appointment", "Call the clinic to book an annual checkup.", new Date(2025, 5, 21), 2, "My Work");
     Task.addTask("Prepare for Meeting", "Review project updates and draft key discussion points.", new Date(2025, 3, 4), 3, "My Work");
 
     Task.addTask("Practice Guitar", "Play for 30 minutes and learn a new chord progression.", new Date(2025, 0, 6), 1, "Hobbies");
-    Task.addTask("Weight Training", "Upper & Lower Body Split training program", new Date(2025, 5, 26), 1, "Hobbies");
     Task.addTask("Watch a Movie", "Relax and watch a sci-fi movie in the evening.", new Date(2025, 1, 12), 1, "Hobbies");
 
     Task.addTask("Read 30 Minutes", "Continue reading 'Clean Code' to improve coding skills.", new Date(2025, 1, 12), 0, "Web Development");
